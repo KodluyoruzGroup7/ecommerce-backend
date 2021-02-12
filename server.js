0f27c00f1833a8da9c0189c138ea9acefc4f0fd1
kicikhaluk <f.halukkicik@outlook.com>
2021-02-13 01:19:19 +0300

    port fixed

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
-
-app.listen(4000, () => console.log('server up'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('server up'));
